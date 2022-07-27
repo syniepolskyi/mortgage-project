161376aa6be33a9f2b90b10084f4f2f9cec79380
syniepolskyi <syniepolskyi@yahoo.com>
2022-07-27 20:23:43 +0300

    Base for work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+function toggleModal(){
+    const modalElem = document.querySelector("[data-modal]")
+    modalElem.classList.toggle("is-hidden")
+}
+
+const closeModalElem = document.querySelector("[data-modal-close]")
+
+closeModalElem.addEventListener("click", toggleModal)
+
