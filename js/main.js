c8854ba38fdf433d0bd2c3e9b7549a78752b4044
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-07-27 21:21:34 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,22 @@ const createBankBtn = document.querySelector(".create-bank-btn")
 
 closeModalElem.addEventListener("click", toggleModal)
 createBankBtn.addEventListener("click", toggleModal)
+
+const banks = [
+  {
+    id: "435tr34wrt",
+    name: "Mono",
+    interestRate: 5,
+    maxLoan: 500000,
+    minPayment: 1000,
+    loanTerm: 12,
+  },
+  {
+    id: "asdfw342rew5",
+    name: "Privat",
+    interestRate: 7,
+    maxLoan: 1000000,
+    minPayment: 5000,
+    loanTerm: 50,
+  },
+];
